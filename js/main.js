c3fecd0c4a84ea22fc04ada329eb15f24678be8b
Jack Jennings <j@ckjennin.gs>
2014-10-22 11:19:55 -0700

    center grid

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-var padding = 60;
 var grid_space = 40;
 var overhang = 10;
 var subdivisions = 2;
+var padding = 60;
 var vertical_count = lines_within(view.size.width);
 var horizontal_count = lines_within(view.size.height);
 
@@ -13,8 +13,16 @@ for (var i=0; i <= horizontal_count; i++) {
   make_horizontal_line(i * grid_space / subdivisions)
 };
 
-function lines_within(size) {
-  return Math.floor((size - padding * 2) / grid_space * subdivisions);
+function lines_within(l) {
+  return Math.floor(grid_size(l) / grid_space * subdivisions);
+}
+
+function grid_size(l) {
+  return maximum_edge(l) - padding;
+}
+
+function grid_offset(l) {
+  return Math.floor(((l - padding * 2) % grid_space) / 2);
 }
 
 function make_vertical_line(x) {
@@ -34,24 +42,36 @@ function stroke_width(n) {
 }
 
 function horizontal_bounds(y) {
-  return [
-    new Point(padding - overhang, padding + y),
-    new Point(view.size.width - padding + overhang, padding + y),
-  ]
+  var from = new Point(padding - overhang, padding + y);
+  var to = new Point(maximum_edge(view.size.width) + overhang, padding + y);
+  return new LineBounds(from, to)
 }
 
 function vertical_bounds(x) {
-  return [
-    new Point(padding + x, padding - overhang),
-    new Point(padding + x, view.size.height - padding + overhang),
-  ]
+  var from = new Point(padding + x, padding - overhang);
+  var to = new Point(padding + x, maximum_edge(view.size.height) + overhang);
+  return new LineBounds(from, to);
+}
+
+function maximum_edge(length) {
+  return length - padding - length % grid_space
 }
 
 function make_line(bounds, strokeWidth) {
   return new Path.Line({
-    from: bounds[0],
-    to: bounds[1],
+    from: bounds.from,
+    to: bounds.to,
     strokeColor: 'white',
     strokeWidth: strokeWidth || 2
   });
+}
+
+function LineBounds(from, to) {
+  var offset = new Point(
+    grid_offset(view.size.width),
+    grid_offset(view.size.height)
+  );
+
+  this.from = new Point(from) + offset;
+  this.to = new Point(to) + offset;
 }
