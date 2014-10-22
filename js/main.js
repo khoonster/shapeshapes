4eec42a780a95daaa93ee82717eda8fe9e322e97
Jack Jennings <j@ckjennin.gs>
2014-10-22 11:01:13 -0700

    add inner lines

--- js/main.js
+++ js/main.js
@@ -1,27 +1,36 @@
 var padding = 60;
 var grid_space = 40;
 var overhang = 10;
+var subdivisions = 2;
 var vertical_count = lines_within(view.size.width);
 var horizontal_count = lines_within(view.size.height);
 
 for (var i=0; i <= vertical_count; i++) {
-  make_vertical_line(i * grid_space)
+  make_vertical_line(i * grid_space / subdivisions)
 };
 
 for (var i=0; i <= horizontal_count; i++) {
-  make_horizontal_line(i * grid_space)
+  make_horizontal_line(i * grid_space / subdivisions)
 };
 
 function lines_within(size) {
-  return Math.floor((size - padding * 2) / grid_space);
+  return Math.floor((size - padding * 2) / grid_space * subdivisions);
 }
 
 function make_vertical_line(x) {
-  return make_line(vertical_bounds(x));
+  return make_line(vertical_bounds(x), stroke_width(x));
 }
 
 function make_horizontal_line(y) {
-  return make_line(horizontal_bounds(y));
+  return make_line(horizontal_bounds(y), stroke_width(y));
+}
+
+function stroke_width(n) {
+  if (n % grid_space) {
+    return 1
+  } else {
+    return 2
+  }
 }
 
 function horizontal_bounds(y) {
@@ -38,11 +47,11 @@ function vertical_bounds(x) {
   ]
 }
 
-function make_line(bounds) {
+function make_line(bounds, strokeWidth) {
   return new Path.Line({
     from: bounds[0],
     to: bounds[1],
     strokeColor: 'white',
-    strokeWidth: 2
+    strokeWidth: strokeWidth || 2
   });
 }
