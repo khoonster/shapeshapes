24234e5e91416d7563a352c0871b2383f76df70e
Jack Jennings <j@ckjennin.gs>
2014-11-08 15:32:23 -0800

    clean up shape iterator function

--- js/main.js
+++ js/main.js
@@ -135,10 +135,13 @@ var SVGPresenter = Group.extend({
 Shape.Custom = Group.extend({
   initialize: function(el, size) {
     Group.prototype.initialize.call(this)
-    
+
     this.path = this.importSVG(el);
     this.path.position = new Point(Math.random() * size.width,
                                    Math.random() * size.height);
+    this.width = this.path.bounds.width;
+    this.height = this.path.bounds.height;
+
 
     this.onFrame = function() {
       this.iterate()
@@ -149,24 +152,26 @@ Shape.Custom = Group.extend({
       length: 0.1 * Math.random()
     });
   },
-  
+
   iterate: function() {
     this.checkBorders()
     this.path.position += this.vector;
   },
-  
+
   checkBorders: function() {
-    var size = view.size;
-    if (this.path.position.x < -this.path.bounds.width) {
-      this.path.position.x = size.width + this.path.bounds.width;
-    } else if (this.path.position.x > size.width + this.path.bounds.width) {
-      this.path.position.x = -this.path.bounds.width;
+    var size = view.size,
+        position = this.path.position;
+
+    if (position.x < -this.width) {
+      position.x = size.width + this.width;
+    } else if (position.x > size.width + this.width) {
+      position.x = -this.width;
     }
-    
-    if (this.path.position.y < -this.path.bounds.height) {
-      this.path.position.y = size.height + this.path.bounds.height;
-    } else if (this.path.position.y > size.height + this.path.bounds.height) {
-      this.path.position.y = -this.path.bounds.height;
+
+    if (position.y < -this.height) {
+      position.y = size.height + this.height;
+    } else if (position.y > size.height + this.height) {
+      position.y = -this.height;
     }
   },
 })
