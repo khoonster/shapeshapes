7a6cd50acb3bdc81f9ebc68aa1d5c151d1c8c608
Jack Jennings <j@ckjennin.gs>
2014-11-08 16:09:37 -0800

    add dragging to objects

--- _ps/modules/shape/custom.js
+++ _ps/modules/shape/custom.js
@@ -7,20 +7,39 @@ Shape.Custom = Group.extend({
                                    Math.random() * size.height);
     this.width = this.path.bounds.width;
     this.height = this.path.bounds.height;
+    this.held = false;
 
+    this.originalVector = new Point({
+      angle: 360 * Math.random(),
+      length: 0.2 * Math.random()
+    });
+
+    this.vector = this.originalVector.clone();
 
     this.onFrame = function() {
-      this.iterate()
+      if (!this.held) {
+        this.iterate()
+      };
     }
 
-    this.vector = new Point({
-      angle: 360 * Math.random(),
-      length: 0.1 * Math.random()
-    });
+    this.onMouseUp = function() {
+      this.held = false;
+    }
+
+    this.onMouseDown = function() {
+      this.vector.length = 0;
+      this.held = true;
+    }
+
+    this.onMouseDrag = function(event) {
+      this.vector = new Point(event.delta);
+      this.path.position += event.delta;
+    }
   },
 
   iterate: function() {
     this.checkBorders()
+    this.modulateVector()
     this.path.position += this.vector;
   },
 
@@ -30,14 +49,24 @@ Shape.Custom = Group.extend({
 
     if (position.x < -this.width) {
       position.x = size.width + this.width;
+      this.held = false;
     } else if (position.x > size.width + this.width) {
       position.x = -this.width;
+      this.held = false;
     }
 
     if (position.y < -this.height) {
       position.y = size.height + this.height;
+      this.held = false;
     } else if (position.y > size.height + this.height) {
       position.y = -this.height;
+      this.held = false;
     }
   },
+  
+  modulateVector: function() {
+    if (this.vector.length > this.originalVector.length) {
+      this.vector.length *= 0.8;
+    };
+  }
 })
